f030e62977db5dc061883258b9b3dbe9e1f0995f
Trist1998 <tristanmwood@hotmail.com>
2020-05-15 18:58:58 +0200

    Checking in start of PGP implementation

--- Java/src/messaging/Compression.java
+++ Java/src/messaging/Compression.java
@@ -4,12 +4,11 @@ public class Compression
 {
     public byte[] compress(byte[] message)
     {
-        return new byte[12];
+        return new byte[12];//TODO Compress
     }
 
     public byte[] decompress(byte[] message)
     {
-        return new byte[12];
+        return new byte[12];//TODO Decompress
     }
-
 }
